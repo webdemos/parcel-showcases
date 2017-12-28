5dcc99c4ec4a1a98173bc5b1d214725bbfcf410d
chendachao <chendachao@outlook.com>
2017-12-28 14:16:13 +0800

    vue

---
+++ vue-demo/app.vue
@@ -0,0 +1,20 @@
+  
+  <template>
+    <div id="vue">
+      <h1>Hello, {{ framework }}</h1>
+    </div>
+  </template>
+
+  <script>
+    export default {
+      data() {
+        return { framework: 'Vue'}
+      }
+    }
+  </script>
+
+  <style lang="scss" scoped>
+    h1 {
+      color: rgb(39, 210, 223);
+    }
+  </style>
---
+++ vue-demo/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue/dist/vue.js';
+import App from './app.vue';
+
+const app = window.addEventListener('load', () => {
+  new Vue({
+   'el': '#vueapp',
+    name: App,
+    components: { App },
+    template: `<App />`
+  });
+});
+
+export default app;
+
+
+
+
