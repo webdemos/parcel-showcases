d0f2b85afdf3530f2b39c3e643ed82977e762233
chendachao <chendachao@outlook.com>
2017-12-28 10:08:21 +0800

    init

---
+++ dynamic-import/someModule.js
@@ -0,0 +1,7 @@
+console.log("someModule.js loaded");
+
+module.exports = {
+  render : function(element){
+    element.innerHTML = "You clicked a button";
+  }
+};
---
+++ hello-world/index.js
@@ -0,0 +1,7 @@
+// import another component
+import main from './main';
+
+document.write("Hello world");
+console.log(13545);
+
+main();
