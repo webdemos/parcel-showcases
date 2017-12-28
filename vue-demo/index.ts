6548442b9a2a0caa763d0e8d9ceb6e74328cb8fe
chendachao <chendachao@outlook.com>
2017-12-28 13:53:31 +0800

    ts

---
+++ vue-demo/index.ts
@@ -0,0 +1,14 @@
+
+const framework = document.getElementById("tsapp");
+
+window.addEventListener('load', () => {
+  
+  framework.style.color = '#f3e96f';
+  framework.style.fontSize = '2rem';
+  framework.style.fontWeight = 'bold';
+  framework.innerHTML = 'Hello, TypeScript!';
+
+});
+
+
+
