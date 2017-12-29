6dc7b4e07e9fb63a2659d1598ce98fa3313fd71a
chendachao <chendachao@outlook.com>
2017-12-29 15:54:15 +0800

    ts

--- ts-demo/index.ts
+++ ts-demo/index.ts
@@ -2,13 +2,11 @@
 const framework = document.getElementById("tsapp");
 
 window.addEventListener('load', () => {
-  
+
   framework.style.color = '#f3e96f';
   framework.style.fontSize = '2rem';
   framework.style.fontWeight = 'bold';
   framework.innerHTML = 'Hello, TypeScript!';
 
 });
-
-
 
